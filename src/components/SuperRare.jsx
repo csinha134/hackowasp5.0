19034595e93575895308867d05bc02e58bf3f5e5
Chayan Sinha <csinha134@gmail.com>
2023-04-16 00:13:25 +0530

    dard

---
+++ src/components/SuperRare.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Card from "./Card";
+import super1 from "../assets/super1.png";
+import super2 from "../assets/super2.png";
+import super3 from "../assets/super3.png";
+import super4 from "../assets/super4.png";
+
+export default function SuperRare() {
+  const data = [
+    {
+      image: super2,
+      series: "Gloop Series",
+      title: "Beige",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
+    {
+      image: super3,
+      series: "Gloop Series",
+      title: "Red Man",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
+    {
+      image: super4,
+      series: "Gloop Series",
+      title: "Green",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
+  ];
+ 
+}
